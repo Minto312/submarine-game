98276863789ac09456f99f36d073dda487b76545
Minto312 <karinto3219@gmail.com>
2024-12-22 12:00:02 +0000

    modify: マップのサイズを変更し、セルの管理を強化

--- game/Map.java
+++ game/Map.java
@@ -1,23 +1,37 @@
 
+import java.util.Vector;
+
+
 public class Map {
-    private static final int SIZE = 5;
-    private int[][] grid;
+    private static final int SIZE = 7;
+    private MapCell[][] grid;
 
     public Map() {
-        grid = new int[SIZE][SIZE];
+        grid = new MapCell[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (i == 0 || j == 0 || i == SIZE - 1 || j == SIZE - 1) {
+                    grid[i][j] = new MapCell(i, j, true);
+                } else {
+                    grid[i][j] = new MapCell(i, j, false);
+                }
+            }
+        }
     }
 
-    public boolean isWithinBounds(int x, int y) {
-        return x >= 0 && x < SIZE && y >= 0 && y < SIZE;
+    public MapCell getCell(int y, int x) {
+        return this.grid[y][x];
     }
 
-    public int[][] calculateAttackRange(int x, int y) {
-        int[][] range = new int[SIZE][SIZE];
+    public int[][] calculateAttackArea(Team team) {
+        Vector<MapCell> attackableCells = new Vector<MapCell>();
         for (int i = -1; i <= 1; i++) {
             for (int j = -1; j <= 1; j++) {
-                if (isWithinBounds(x + i, y + j)) {
+                    MapCell cell = grid[x + i][y + j];
+                    if (!cell.isBlocked() && cell.getSubmarine() == null) {
+                        attackableCells.add(cell);
+                    }
                     range[x + i][y + j] = 1;
-                }
             }
         }
         return range;
