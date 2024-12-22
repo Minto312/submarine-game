fdb2d57a80a952425d345a2d785dc9a439a8018b
Minto312 <karinto3219@gmail.com>
2024-12-22 20:22:04 +0000

    feat: ComputerTeamクラスを追加し、Teamクラスを抽象化して戦略メソッドを定義

---
+++ game/ComputerTeam.java
@@ -0,0 +1,12 @@
+
+public class ComputerTeam extends Team {
+    public ComputerTeam(Map map, int teamId) {
+        super(map, teamId);
+    }
+
+    public void takeTurn(Map map) {
+        // ここに戦略を書く
+        
+    }
+    
+}
--- game/Team.java
+++ game/Team.java
@@ -1,11 +1,13 @@
 import java.util.Vector;
 
 
-class Team {
+public abstract class Team {
     private static final int SUBMARINE_COUNT = 4;
+    private int teamId;
     private Vector<Submarine> submarineList;
 
-    public Team(Map map) {
+    public Team(Map map, int teamId) {
+        this.teamId = teamId;
         submarineList = new Vector<Submarine>(SUBMARINE_COUNT);
         for (int i = 0; i < SUBMARINE_COUNT; i++) {
             MapCell startCell = map.getCell(0, i); // strategyで決める
@@ -16,13 +18,27 @@ class Team {
         }
     }
 
-    public void makeMove(int index, int newX, int newY) {
-        if (index >= 0 && index < submarineList.size()) {
-            submarineList.get(index).move(newX, newY);
+    public Vector<Submarine> getSubmarineList() {
+        return submarineList;
+    }
+
+    public void move(char submarineCode, String cellCode, Map map) {
+        int[] res = Util.parseCellCode(cellCode);
+        int y = res[0];
+        int x = res[1];
+
+        MapCell toCell = map.getCell(y, x);
+        for (Submarine submarine : submarineList) {
+            if (submarine.getCode() == submarineCode) submarine.move(toCell, this.teamId);
         }
     }
-        if (index >= 0 && index < submarines.length) {
-            submarines[index].move(newX, newY);
-        }
+
+    public void attack(String cellCode, Map map) {
+        int[] res = Util.parseCellCode(cellCode);
+        int y = res[0];
+        int x = res[1];
+
     }
+
+    public abstract void takeTurn(Map map);
 }
