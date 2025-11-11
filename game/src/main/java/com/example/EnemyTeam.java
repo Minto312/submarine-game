6e14149e6d46c725a458017e6a85b36b33edb192
Minto312 <karinto3219@gmail.com>
2025-11-11 22:17:58 +0900

    wip

---
+++ game/src/main/java/com/example/EnemyTeam.java
@@ -0,0 +1,14 @@
+
+public class EnemyTeam extends Team {
+
+    public EnemyTeam(Map map, int teamId) {
+        super(map, teamId);
+    }
+
+    @Override
+    public Log takeTurn(Game game) {
+        Sccaner scanner = new Scanner(System.in);
+        in
+
+    }
+}
